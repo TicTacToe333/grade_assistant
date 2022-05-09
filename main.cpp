1cfc5f2669f3007e28932d898c5cd8bc14fc855b
TicTacToe333 <aidencedwards@gmail.com>
2022-05-09 12:07:18 -0700

    fixed issues

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
-// Your First C++ Program
-
 #include <iostream>
+#include <vector>
+#include <string>
+
 using namespace std;
-int main() {
-    std::cout << "Hello World!";
-    return 0;
+
+int main()
+{
+    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+
+    for (const string& word : msg)
+    {
+        cout << word << " ";
+    }
+    cout << endl;
 }
